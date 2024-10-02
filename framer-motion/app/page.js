879817905e0fcbce1fced4c9b40ccaa0c8010f1b
Udit Kapoor <uditkapoor060@gmail.com>
2024-10-02 21:15:29 +0530

    initialization of project

---
+++ framer-motion/app/page.js
@@ -0,0 +1,9 @@
+import AnimationControls from "@/components/AnimationControls";
+import BasicsOfMotion from "../components/BasicsOfMotion";
+import Gestures from "../components/Gestures";
+import ScrollAnimations from "../components/ScrollAnimation";
+import ViewBasedAnimations from "../components/ViewBaseAnimaions";
+
+export default function Home() {
+  return <div></div>;
+}
